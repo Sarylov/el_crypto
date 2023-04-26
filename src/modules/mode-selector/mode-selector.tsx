d05ded4a9102bb46d1d71b934a17dfeac94a681f
Sarylov <jalsan.work@yandex.ru>
2023-04-26 23:17:20 +0300

    change mode-selector

--- src/modules/mode-selector/mode-selector.tsx
+++ src/modules/mode-selector/mode-selector.tsx
@@ -1,13 +1,27 @@
+import modeSelectorState from "../../store/mode-selector-state";
 import { observer } from "mobx-react-lite";
-import modeSelector from "../../store/mode-selector";
+import { Button, ButtonGroup } from "@mui/material";
 
 export const ModeSelector: React.FC = observer(() => {
-  console.log(modeSelector.selected);
+  const { selected, selectEncrypt, selectDecrypt } = modeSelectorState;
+
+  const boundSelectEncrypt = selectEncrypt.bind(modeSelectorState);
+  const boundSelectDecrypt = selectDecrypt.bind(modeSelectorState);
 
   return (
-    <>
-      <button onClick={() => modeSelector.selectEncrypt()}>шифровать</button>
-      <button onClick={() => modeSelector.selectDecrypt()}>расшифровать</button>
-    </>
+    <ButtonGroup>
+      <Button
+        variant={selected === "encrypt" ? "contained" : "outlined"}
+        onClick={boundSelectEncrypt}
+      >
+        Зашифровать
+      </Button>
+      <Button
+        variant={selected === "decrypt" ? "contained" : "outlined"}
+        onClick={boundSelectDecrypt}
+      >
+        Расшифровать
+      </Button>
+    </ButtonGroup>
   );
 });
