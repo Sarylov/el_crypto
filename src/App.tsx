5211c052f8fe90f6425f88cb22391219752df635
Sarylov <jalsan.work@yandex.ru>
2023-05-01 15:40:25 +0300

    add feedback to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Stack, Container } from "@mui/material";
 import { ModeSelector } from "./modules/mode-selector";
 import { Cryptographer } from "./modules/cryptographer";
+import { Feetback } from "./modules/feetback";
 
 function App() {
   return (
     <Container maxWidth="md">
+      <Feetback />
+
       <Stack
         height="100vh"
         spacing={2}
