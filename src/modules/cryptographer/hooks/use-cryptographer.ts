06bde24795dc8515a7ce782086c58d7a6e45031f
Sarylov <jalsan.work@yandex.ru>
2023-05-01 15:39:59 +0300

    add feetback action

--- src/modules/cryptographer/hooks/use-cryptographer.ts
+++ src/modules/cryptographer/hooks/use-cryptographer.ts
@@ -2,6 +2,34 @@
 import { gost89 } from "../../../helpers/gost89";
 import { getRandomInteger } from "../../../helpers/random";
 import cryptographerState from "../../../store/cryptographer-state";
+import feetbackState from "../../../store/feetback-state";
+import { IFeetBack } from "../../../types";
+
+const successfullyEncryptedFeedback: IFeetBack = {
+  type: "success",
+  title: "Зашифрованно",
+  message: "Текст успешно зашифрован",
+};
+
+const unsuccessfullyEncryptedFeedback: IFeetBack = {
+  type: "error",
+  title: "Не удалось зашифровать",
+  message:
+    "Проверьте правильность формата ключа. Ключ должен состоять из 8 целых чисел разделенных пробелом",
+};
+
+const successfullyDecryptedFeedback: IFeetBack = {
+  type: "success",
+  title: "Расшифрованно",
+  message: "Текст успешно расшифрован",
+};
+
+const unsuccessfullyDecryptedFeedback: IFeetBack = {
+  type: "error",
+  title: "Не удалось зашифровать",
+  message:
+    "Проверьте правильность формата ключа. Ключ должен состоять из 8 целых чисел разделенных пробелом",
+};
 
 export const useCryptographer = () => {
   const {
@@ -21,20 +49,33 @@ export const useCryptographer = () => {
   const boundSetKey = setKey.bind(cryptographerState);
   const boundSetDecryptedText = setDecryptedText.bind(cryptographerState);
   const boundSetDecryptionKey = setDecryptionKey.bind(cryptographerState);
+  const boundSetFeetback = feetbackState.setFeetback.bind(feetbackState);
 
   function encrypt() {
-    const keyArr: number[] = key.split(" ").map((keyItem) => parseInt(keyItem));
-    const encryptedText: string = gost89.encrypt(initialText, keyArr);
-    boundSetEncryptedText(encryptedText);
-    boundSetDecryptionKey(key);
+    try {
+      const keyArr: number[] = key
+        .split(" ")
+        .map((keyItem) => parseInt(keyItem));
+      const encryptedText: string = gost89.encrypt(initialText, keyArr);
+      boundSetEncryptedText(encryptedText);
+      boundSetDecryptionKey(key);
+      boundSetFeetback(successfullyEncryptedFeedback);
+    } catch (error) {
+      boundSetFeetback(unsuccessfullyEncryptedFeedback);
+    }
   }
 
   function decrypt() {
-    const keyArr: number[] = decryptionKey
-      .split(" ")
-      .map((keyItem) => parseInt(keyItem));
-    const decryptedText: string = gost89.decrypt(encryptedText, keyArr);
-    boundSetDecryptedText(decryptedText);
+    try {
+      const keyArr: number[] = decryptionKey
+        .split(" ")
+        .map((keyItem) => parseInt(keyItem));
+      const decryptedText: string = gost89.decrypt(encryptedText, keyArr);
+      boundSetDecryptedText(decryptedText);
+      boundSetFeetback(successfullyDecryptedFeedback);
+    } catch (error) {
+      boundSetFeetback(unsuccessfullyDecryptedFeedback);
+    }
   }
 
   function copyEncryptedText() {
